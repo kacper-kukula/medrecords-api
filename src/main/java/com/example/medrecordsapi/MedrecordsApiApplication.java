f56b5148e0ac1b909a3b94179ab25789cd6b9ce4
Kacper <kacperkukula97@gmail.com>
2025-01-09 16:26:48 +0100

    Initial project setup with Spring Boot, MongoDB and checkstyle ruleset

---
+++ src/main/java/com/example/medrecordsapi/MedrecordsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.medrecordsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedrecordsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedrecordsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medrecordsapi/MedrecordsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medrecordsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedrecordsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

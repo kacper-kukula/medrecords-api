1a4bede220e0c82aea1059a5b020ee1becc38a7f
Kacper <kacperkukula97@gmail.com>
2025-01-11 17:01:15 +0100

    Add Secret to GitHub Repo

--- src/main/java/com/example/medrecordsapi/exception/custom/UnauthorizedViewException.java
+++
@@ -1,8 +0,0 @@
-package com.example.medrecordsapi.exception.custom;
-
-public class UnauthorizedViewException extends RuntimeException {
-
-    public UnauthorizedViewException(String message) {
-        super(message);
-    }
-}
